442bcb6af4ad147c8917204f4555ceecd57b4cf2
hir0o <mk.9.q.kyu@gmail.com>
2022-12-15 20:30:36 +0900

    feat: Headerをコンポーネント化

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,18 @@ import { FC } from "react";
 
 type Props = {
   title: React.ReactNode;
-  children: React.ReactNode;
   back?: boolean;
 };
 
-export const Header: FC<Props> = ({ title, children, back }) => {
+export const Header: FC<Props> = ({ title, back }) => {
   return (
     <header className="flex flex-col justify-center items-start gap-3">
-      {children}
+      <div>
+        <div className="flex ">
+          <h1 className="text-white font-bold">{title}</h1>
+        </div>
+        <button className="text-blue-300 text-sm underline">新規作成</button>
+      </div>
     </header>
   );
 };
--- src/pages/Top.tsx
+++ src/pages/Top.tsx
@@ -3,6 +3,7 @@ import { CollectionDetail } from "../components/CollectiponDetail";
 import { FolderList } from "../components/FolderList";
 import { Header } from "../components/Header";
 import { Collection, useCollection } from "../useCollection";
+import { Layout } from "./Layout";
 
 export const Top: FC = () => {
   const { collections } = useCollection();
@@ -24,40 +25,14 @@ export const Top: FC = () => {
   }, []);
 
   return (
-    <div className="bg-bg p-2 flex flex-col gap-3">
-      <Header title={title}>
-        <div>
-          <>
-            <div className="flex ">
-              {selectedCollection !== undefined && (
-                <button
-                  type="button"
-                  onClick={() => setSelectedCollection(undefined)}
-                  className="mr-2 text-white"
-                >
-                  &lt;
-                </button>
-              )}
-              <h1 className="text-white font-bold">{title}</h1>
-            </div>
-            {selectedCollection === undefined ? (
-              <button className="text-blue-300 underline">
-                + 新しいコレクションを追加する
-              </button>
-            ) : (
-              <button className="text-blue-300 underline" onClick={handleClick}>
-                + 現在のページを保存する
-              </button>
-            )}
-          </>
-        </div>
-      </Header>
+    <Layout>
+      <Header title={title} />
       <hr className="border-gray-500 border-opacity-50 border-1" />
       {selectedCollection ? (
         <CollectionDetail collection={selectedCollection} />
       ) : (
         <FolderList onSelect={handleSelect} collections={collections} />
       )}
-    </div>
+    </Layout>
   );
 };
